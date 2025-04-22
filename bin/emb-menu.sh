7d29d8178243e01d95786c1e56a036e7b71780cd
Matt St. Onge <matt_st_onge@yahoo.com>
2025-04-22 13:32:37 -0400

    added emb-menu.sh

---
+++ bin/emb-menu.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+################################
+#
+# example first boot menu system
+#
+# MIT License
+#
+###############################
+
+source ../conf/emb_fb.conf
+
+clear
+
+echo "*****************************"
+echo "*                           *"
+echo "*    WELCOME TO YOUR        *"
+echo "*                           *"
+echo "*    NEW APPLIANCE          *"
+echo "*                           *"
+echo "*                           *"
+echo "*****************************"
+echo ; echo ; echo
+echo " PRESS ANY KEY to CONTINUE"
+
+
+
