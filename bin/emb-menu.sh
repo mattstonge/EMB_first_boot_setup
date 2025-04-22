92b8b90717997898f6c9b8622e11250bb6e0eec0
Matt St. Onge <matt_st_onge@yahoo.com>
2025-04-22 13:45:01 -0400

    updated emb-menu.sh

--- bin/emb-menu.sh
+++ bin/emb-menu.sh
@@ -8,20 +8,52 @@
 #
 ###############################
 
-source ../conf/emb_fb.conf
 
-clear
 
-echo "*****************************"
-echo "*                           *"
-echo "*    WELCOME TO YOUR        *"
-echo "*                           *"
-echo "*    NEW APPLIANCE          *"
-echo "*                           *"
-echo "*                           *"
-echo "*****************************"
-echo ; echo ; echo
-echo " PRESS ANY KEY to CONTINUE"
+
+
+
+function ReadMyDefaults()
+{
+
+	source ../conf/emb_fb.conf
+
+	Check4file
+}
+
+
+
+function Check4file()
+{
+
+	#if the file exists StartMyMenu
+	#if it doesn't exist Exit0
+
+### insert file checker 
+
+}
+
+
+
+function StartMyMenu()
+{
+
+	clear
+
+	echo "*****************************"
+	echo "*                           *"
+	echo "*    WELCOME TO YOUR        *"
+	echo "*                           *"
+	echo "*    NEW APPLIANCE          *"
+	echo "*                           *"
+	echo "*                           *"
+	echo "*****************************"
+	echo ; echo ; echo
+	echo " PRESS ANY KEY to CONTINUE"
+
+	input $MYPROMPT
+
+}
 
 
 
