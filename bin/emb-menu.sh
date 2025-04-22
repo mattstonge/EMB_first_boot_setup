9b544f19e55a7f284f2ecfb52c3ed22a5bd6a597
Matt St. Onge <matt_st_onge@yahoo.com>
2025-04-22 14:26:31 -0400

    updated emb-menu.sh

--- bin/emb-menu.sh
+++ bin/emb-menu.sh
@@ -57,3 +57,31 @@ function StartMyMenu()
 
 
 
+function MainMenuPage()
+{
+
+	clear
+
+        echo "*****************************"
+        echo "*                           *"
+        echo "* 1)  Admin Password        *"
+        echo "*                           *"
+        echo "* 2)  Set Hostname          *"
+        echo "*                           *"
+        echo "* 3)  Configure Network     *"
+        echo "*                           *"
+        echo "* 4)  Factory Reset         *"
+        echo "*                           *"
+        echo "*                           *"
+        echo "*                           *"
+        echo "*                           *"
+        echo "* 0)  Exit                  *"
+        echo "*                           *"
+        echo "*****************************"
+        echo ; echo ; echo
+        echo " PRESS ANY KEY to CONTINUE"
+
+# insert input/processing case statement
+
+}
+
